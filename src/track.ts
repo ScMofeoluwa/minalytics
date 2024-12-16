c663ed26ab139a967ebdc9e87c22aae029cae1e2
Mofeoluwa Lijadu <lijsamobafunmilayo@gmail.com>
2024-12-16 08:57:41 +0100

    fix: little changes
    fix: little changes

--- src/track.ts
+++ src/track.ts
@@ -1,45 +1,71 @@
 import { createHash } from "crypto";
 
 interface ITrackingData {
-  url: string;
+  url?: string;
   visitorId: string,
-  referrer: string | null
-  deviceType: string,
-  browserName: string,
-  os: string
+  referrer: string | null,
+  ua: string,
+  details?: Record<string, any>
 }
 
-interface TrackPayload {
+interface EventPayload {
   tracking: ITrackingData,
   type: string
 }
 
 class Analytics {
   private visitorId: string;
+  private referrer: string | null;
 
   constructor(){
     this.visitorId = this.generateDailyVisitorHash()
+    this.referrer = this.getReferrer()
   }
 
-  public track(type: string = 'pageview') {
-    const ref = document.referrer
-    const externalReferrer = ref && new URL(ref).host !== window.location.hostname ? ref : null
-
+  public track(type: string = 'pageview', details?: Record<string, any>) {
     const trackingData: ITrackingData = {
-      url: window.location.href,
       visitorId: this.visitorId,
-      referrer: externalReferrer,
-      deviceType: this.getDeviceType(),
-      browserName: this.getBrowserName(),
-      os: this.getOS()
+      referrer: this.referrer,
+      ua: navigator.userAgent,
+    }
+    
+    if (type === "payload"){
+      trackingData.url = window.location.href
     }
 
-    const payload: TrackPayload = {
+    if (details) {
+      trackingData.details = details
+    }
+
+    const payload: EventPayload = {
       tracking: trackingData,
       type: type
     }
 
     this.sendData(payload)
+  }
+
+  public trackSubsequentPages(){
+    const originalPushState = window.history.pushState
+    const originalReplaceState = window.history.replaceState
+
+    window.history.pushState = (...args) => {
+      originalPushState.apply(this, args)
+      this.track()
+    }
+
+    window.history.replaceState = (...args) => {
+      originalReplaceState.apply(this, args)
+      this.track()
+    }
+
+    window.addEventListener('popstate', () => {
+      this.track()
+    })
+
+    // window.addEventListener('hashchange', () => {
+    //   this.track(document.location.hash)
+    // })
   }
 
   private generateDailyVisitorHash(): string{
@@ -51,62 +77,16 @@ class Analytics {
     return this.createHash(components)
   }
 
-  private getDeviceType(): string{
-    const isTablet = window.matchMedia('(min-width: 768px) and (max-width: 1024px)').matches
-    const isMobile = window.matchMedia('(max-width: 767px)').matches || ('maxTouchPoints' in navigator && navigator.maxTouchPoints > 0)
-
-    if (isTablet) return 'tablet'
-    if (isMobile) return 'mobile'
-    return 'desktop'
-  }
-
-  private getBrowserName(): string{
-    const userAgentData = (navigator as any).userAgentData
-    const ua = navigator.userAgent
-
-    if (userAgentData?.brands) {
-      const brand = userAgentData.brands.find(
-        (b: {brand: string}) => !["Chromium", "Not-A.Brand"].includes(b.brand)
-      )
-      if (brand) return brand.brand
-    }
-
-    if (ua.includes("Firefox/")){
-      return 'Firefox'
-    } else if ((window as any).chrome) {
-      if (ua.includes("Edg/")) return "Edge"
-      if (ua.includes("OPR/")) return "Opera"
-      return "Chrome"
-    } else if (ua.includes("Safari/") && !ua.includes("Chrome/")){
-      return "Safari"
-    }
-    return "Unknown"
-  }
-
-  private getOS(): string {
-    const userAgentData = (navigator as any).userAgentData
-
-    if (userAgentData?.platform){
-      return userAgentData.platform
-    }
-
-    const platform = navigator.platform
-
-    if (platform.includes("Win")) return "Window" 
-    if (platform.includes("Mac")) return "MacOS" 
-    if (/iPhone|iPad|iPod/.test(platform)) return "iOS"
-    if (platform.includes("Linux")) {
-      return navigator.userAgent.includes("Android") ? "Android": "Linux"
-    }
-    
-    return "Unknown"
-  }
-
   private createHash(input: string): string{
     return createHash('sha1').update(input).digest('hex')
   }
 
-  private sendData(payload: TrackPayload){
+  private getReferrer() {
+    const ref = document.referrer
+    return ref && new URL(ref).host !== window.location.hostname ? ref : null
+  }
+
+  private sendData(payload: EventPayload){
     const s = JSON.stringify(payload)
     const url = `http://localhost:3000/track?data=${btoa(s)}`
 
@@ -114,3 +94,11 @@ class Analytics {
     img.src = url;
   }
 }
+
+((w,d) => {
+  const analytics = new Analytics()
+  analytics.track()
+  analytics.trackSubsequentPages()
+
+  w._analytics = analytics
+})
